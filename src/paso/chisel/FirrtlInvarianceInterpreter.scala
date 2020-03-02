a810f0e657c4ad8bc940ccfa9dfdf33f79272606
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-03-02 14:51:09 -0800

    split asserts

--- src/paso/chisel/FirrtlInvarianceInterpreter.scala
+++ src/paso/chisel/FirrtlInvarianceInterpreter.scala
@@ -14,7 +14,13 @@ case class Assertion(guard: smt.Expr, pred: smt.Expr)
 
 class FirrtlInvarianceInterpreter(circuit: ir.Circuit, annos: Seq[Annotation]) extends PasoFirrtlInterpreter(circuit, annos) {
   val asserts = mutable.ArrayBuffer[Assertion]()
+
+  def makeAsserts(guard: smt.Expr, pred: smt.Expr): Seq[Assertion] = pred match {
+    case smt.OperatorApplication(smt.ConjunctionOp, List(a,b)) => makeAsserts(guard, a) ++ makeAsserts(guard, b)
+    case other => Seq(Assertion(guard, other))
+  }
+
   override def onAssert(expr: smt.Expr): Unit = {
-    asserts.append(Assertion(pathCondition, expr))
+    asserts ++= makeAsserts(pathCondition, expr)
   }
 }
