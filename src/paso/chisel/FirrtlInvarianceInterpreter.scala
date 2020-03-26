c30b7fca684d6cf04a6999945c33b5ea331e1313
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-03-26 15:49:32 -0700

    automagically detect and rewrite complete array comparisons

--- src/paso/chisel/FirrtlInvarianceInterpreter.scala
+++ src/paso/chisel/FirrtlInvarianceInterpreter.scala
@@ -29,6 +29,40 @@ class FirrtlInvarianceInterpreter(circuit: ir.Circuit, annos: Seq[Annotation]) e
   }
 
   override def onAssert(expr: smt.Expr): Unit = {
-    asserts ++= makeAsserts(pathCondition, substituteSmt(expr, vecAsMem))
+    val e = substituteSmt(mergeArrayEquality(expr), vecAsMem)
+    asserts ++= makeAsserts(pathCondition, e)
+  }
+
+  /* this deals with the fact that Chisel expands comparisons between Vec into element wise comparisons */
+  private def mergeArrayEquality(e: smt.Expr): smt.Expr = {
+    val clauses = conjunctionToSeq(e)
+    val arrayEq = clauses.map(parseArrayEq)
+    val allArrayEq = arrayEq.forall(_.nonEmpty)
+    if(allArrayEq) {
+      val (a0, a1, _) = arrayEq.head.get
+      assert(a0.typ == a1.typ)
+      val depth = getArrayDepth(a0.typ)
+      if(arrayEq.size == depth) {
+        val complete = arrayEq.zipWithIndex.forall {
+          case (Some((b0, b1, addr)), i) => b0 == a0 && b1 == a1 && addr.toInt == i
+        }
+        if (complete) {
+          smt.OperatorApplication(smt.EqualityOp, List(a0, a1))
+        } else { e }
+      } else { e }
+    } else { e }
+  }
+  private def parseArrayEq(e: smt.Expr): Option[(smt.Symbol, smt.Symbol, BigInt)] = e match {
+    case smt.OperatorApplication(smt.EqualityOp, List(
+      smt.ArraySelectOperation(a0: smt.Symbol, List(smt.BitVectorLit(i0, _))),
+      smt.ArraySelectOperation(a1: smt.Symbol, List(smt.BitVectorLit(i1, _))))) if i0 == i1 => Some((a0, a1, i0))
+    case _ => None
+  }
+  private def conjunctionToSeq(e: smt.Expr): Seq[smt.Expr] = e match {
+    case smt.OperatorApplication(smt.ConjunctionOp, List(a,b)) => conjunctionToSeq(a) ++ conjunctionToSeq(b)
+    case other => Seq(other)
+  }
+  private def getArrayDepth(typ: smt.Type): BigInt = typ match {
+    case smt.ArrayType(List(smt.BitVectorType(w)), _) => BigInt(1) << w
   }
 }
