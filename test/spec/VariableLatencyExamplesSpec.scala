4628b48be39009819706940e864e9e144d71d9a2
Kevin Laeufer <ekiwi@berkeley.edu>
2020-04-09 15:54:38 -0700

    try to verify variable latency keep output circuit

--- test/spec/VariableLatencyExamplesSpec.scala
+++ test/spec/VariableLatencyExamplesSpec.scala
@@ -14,6 +14,19 @@ class Identity extends UntimedModule {
   val idle = fun("idle"){}
 }
 
+class IdentityAndKeep extends UntimedModule {
+  val valid = RegInit(false.B)
+  val value = Reg(UInt(32.W))
+  val id = fun("id").in(UInt(32.W)).out(UInt(32.W)){ (in, out) =>
+    out := in
+    valid := true.B
+    value := in
+  }
+  val idle = fun("idle").out(UInt(32.W)){ out =>
+    when(valid) { out := value } .otherwise { out := DontCare }
+  }
+}
+
 class VariableLatencyIO extends Bundle {
   val start = Input(Bool())
   val done = Output(Bool())
@@ -28,7 +41,7 @@ class RandomLatency(maxLatency: Int = 4) extends VariableLatencyModule {
   when(io.start) { buffer := io.dataIn }
 
   val counterTyp = UInt(log2Ceil(maxLatency).W)
-  val delay = Reg(counterTyp)
+  val delay = RegInit(counterTyp, 1.U) // unfortunately yosys compiles an uninitialized Reg into an input when emitting btor2, why?
   val counter = Reg(counterTyp)
   counter := Mux(io.start, 0.U, counter + 1.U)
 
@@ -76,8 +89,42 @@ class RandomLatencyInductive(impl: RandomLatency, spec: Identity) extends Random
   invariances { dut => assert(!dut.running)  }
 }
 
+class VariableLatencyKeepProtocols[F <: VariableLatencyModule](impl: F, spec: IdentityAndKeep) extends Binding(impl, spec) {
+  protocol(spec.id)(impl.io) { (clock, dut, in, out) =>
+    dut.start.poke(true.B)
+    dut.dataIn.poke(in)
+    dut.done.expect(false.B)
+    clock.step()
+
+    dut.start.poke(false.B)
+    dut.dataIn.poke(DontCare)
+    do_while(!dut.done.peek(), max=4) {
+      clock.step()
+    }
+
+    dut.dataOut.expect(out)
+  }
+  protocol(spec.idle)(impl.io) { (clock, dut, out) =>
+    dut.start.poke(false.B)
+    dut.dataOut.expect(out)
+    clock.step()
+  }
+}
+
+class VariableLatencyKeepInductive(impl: RandomLatencyKeepOutput, spec: IdentityAndKeep) extends VariableLatencyKeepProtocols(impl, spec) {
+  invariances { dut => assert(!dut.running)  }
+  mapping { (impl, spec) => when(spec.valid) { assert(impl.buffer === spec.value) } }
+}
+
 
 class VariableLatencyExamplesSpec extends FlatSpec {
-  val p = Elaboration[RandomLatency, Identity](new RandomLatency, new Identity, (impl, spec) => new RandomLatencyInductive(impl, spec))
-  VerificationProblem.verify(p)
+  "RandomLatency module" should "refine its spec" in {
+    val p = Elaboration[RandomLatency, Identity](new RandomLatency, new Identity, (impl, spec) => new RandomLatencyInductive(impl, spec))
+    VerificationProblem.verify(p)
+  }
+
+  "RandomLatencyAndKeep module" should "refine its spec" in {
+    val p = Elaboration[RandomLatencyKeepOutput, IdentityAndKeep](new RandomLatencyKeepOutput, new IdentityAndKeep, (impl, spec) => new VariableLatencyKeepInductive(impl, spec))
+    VerificationProblem.verify(p)
+  }
 }
