ffb40611f0ad972f78c00341078bdf3f786d7f49
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-04-16 11:30:58 -0700

    add description to memory model

--- test/spec/FPGAMemoriesSpec.scala
+++ test/spec/FPGAMemoriesSpec.scala
@@ -12,15 +12,23 @@ import org.scalatest._
 import paso.chisel.Elaboration
 import paso.verification.VerificationProblem
 
+// NOTE: while the spec is currently hard coded for a certain number of read and write ports, one
+//       could write a spec generator that takes these numbers as inputs and generates the correct spec.
 
-class In(val depth: BigInt, val w: Int) extends Bundle {
-  val readAddr = UInt(log2Ceil(depth).W)
-  val writeAddr = UInt(log2Ceil(depth).W)
-  val writeData = UInt(w.W)
-}
+/////////////////////////////// 1W 1R ///////////////////////
+/**
+ * A memory with one read one write port.
+ * - A read/write conflict returns the new value (other options would be to return the old value or an undefined result)
+ * - Reading from a location that has not been written to results in an undefined (arbitrary) result.
+ *  */
 class Untimed1W1RMemory(size: MemSize) extends UntimedModule {
   val mem = Mem(size.depth, size.dataType)
   val valid = Mem(size.depth, Bool())
+  class In(val depth: BigInt, val w: Int) extends Bundle {
+    val readAddr = UInt(log2Ceil(depth).W)
+    val writeAddr = UInt(log2Ceil(depth).W)
+    val writeData = UInt(w.W)
+  }
   val rw = fun("rw").in(new In(size.depth, size.dataType.getWidth)).out(size.dataType) { (in, readData) =>
     when(valid(in.readAddr)) {
       when(in.readAddr === in.writeAddr) {
@@ -62,5 +70,9 @@ class FPGAMemoriesSpec extends FlatSpec {
     val data = MemData(MemSize(UInt(32.W), 32), 1, 1)
     val p = Elaboration()[SimulationMem, Untimed1W1RMemory](new SimulationMem(data), new Untimed1W1RMemory(data.size), (impl, spec) => new Simulation1W1RInductive(impl, spec))
     VerificationProblem.verify(p)
+  }
+
+  "Charles Eric LaForest LVT 2W4R memory" should "refine its spec" in {
+    // TODO
   }
 }
