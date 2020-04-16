76234d3500bc89cb409e456fbc6feb6699e1788f
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-04-16 13:53:38 -0700

    add 2W/4R mem spec

--- test/spec/FPGAMemoriesSpec.scala
+++ test/spec/FPGAMemoriesSpec.scala
@@ -6,7 +6,7 @@ package spec
 
 import chisel3._
 import chisel3.util._
-import impl.{FPGAMem, MemData, MemSize, SimulationMem}
+import impl.{ParallelWriteMem, FPGAMem, LVTMemory, MemData, MemSize, SimulationMem}
 import paso._
 import org.scalatest._
 import paso.chisel.Elaboration
@@ -17,7 +17,7 @@ import paso.verification.VerificationProblem
 
 /////////////////////////////// 1W 1R ///////////////////////
 /**
- * A memory with one read one write port.
+ * A memory with one read and one write port.
  * - A read/write conflict returns the new value (other options would be to return the old value or an undefined result)
  * - Reading from a location that has not been written to results in an undefined (arbitrary) result.
  *  */
@@ -63,6 +63,121 @@ class Simulation1W1RInductive(impl: SimulationMem, spec: Untimed1W1RMemory) exte
   }
 }
 
+/////////////////////////////// 2W 4R ///////////////////////
+
+/**
+ * A memory with four read and two write ports.
+ * - A read/write conflict returns the new value (other options would be to return the old value or an undefined result)
+ * - Reading from a location that has not been written to results in an undefined (arbitrary) result.
+ * - A write/write conflict results in an undefined (arbitrary) value being written to the location.
+ *  */
+class Untimed2W4RMemory(size: MemSize) extends UntimedModule {
+  val mem = Mem(size.depth, size.dataType)
+  val valid = Mem(size.depth, Bool())
+  class In(val addrWidth: Int, val dataWidth: Int) extends Bundle {
+    val readAddr0 = UInt(addrWidth.W)
+    val readAddr1 = UInt(addrWidth.W)
+    val readAddr2 = UInt(addrWidth.W)
+    val readAddr3 = UInt(addrWidth.W)
+    val writeAddr0 = UInt(addrWidth.W)
+    val writeData0 = UInt(dataWidth.W)
+    val writeAddr1 = UInt(addrWidth.W)
+    val writeData1 = UInt(dataWidth.W)
+  }
+  class Out(val dataWidth: Int) extends Bundle {
+    val readData0 = UInt(dataWidth.W)
+    val readData1 = UInt(dataWidth.W)
+    val readData2 = UInt(dataWidth.W)
+    val readData3 = UInt(dataWidth.W)
+  }
+
+  def read(addr: UInt, data: UInt, in: In): Unit = {
+    when(addr === in.writeAddr0) {
+      data := in.writeData0
+    } .elsewhen(addr === in.writeAddr1) {
+      data := in.writeData1
+    } .elsewhen(valid(addr)) {
+      data := mem(addr)
+    } .otherwise {
+      data := DontCare
+    }
+  }
+
+  val rw = fun("rw").in(new In(log2Ceil(size.depth), size.dataType.getWidth)).out(new Out(size.dataType.getWidth)) { (in, out) =>
+    // read
+    read(in.readAddr0, out.readData0, in)
+    read(in.readAddr1, out.readData1, in)
+    read(in.readAddr2, out.readData2, in)
+    read(in.readAddr3, out.readData3, in)
+
+    // write
+    when(in.writeAddr0 === in.writeAddr1) {
+      valid(in.writeAddr0) := false.B
+    } .otherwise {
+      mem(in.writeAddr0) := in.writeData0
+      valid(in.writeAddr0) := true.B
+      mem(in.writeAddr1) := in.writeData1
+      valid(in.writeAddr1) := true.B
+    }
+  }
+}
+
+// While the protocol does not depend on the implementation, it does depend on the number of read and write ports.
+// We could turn the protocol into a generator and make it generic vis-a-vis the number of ports.
+class Mem2W4RProtocol[F <: FPGAMem](impl: F, spec: Untimed2W4RMemory) extends Binding(impl, spec) {
+  protocol(spec.rw)(impl.io) { (clock, dut, in, out) =>
+    // write
+    dut.write(0).addr.poke(in.writeAddr0)
+    dut.write(0).data.poke(in.writeData0)
+    dut.write(1).addr.poke(in.writeAddr1)
+    dut.write(1).data.poke(in.writeData1)
+    //read
+    dut.read(0).addr.poke(in.readAddr0)
+    dut.read(1).addr.poke(in.readAddr1)
+    dut.read(2).addr.poke(in.readAddr2)
+    dut.read(3).addr.poke(in.readAddr3)
+
+    clock.step()
+
+    // invalidate inputs (TODO: add option to make pokes not stick!)
+    dut.write(0).addr.poke(DontCare)
+    dut.write(0).data.poke(DontCare)
+    dut.write(1).addr.poke(DontCare)
+    dut.write(1).data.poke(DontCare)
+    dut.read(0).addr.poke(DontCare)
+    dut.read(1).addr.poke(DontCare)
+    dut.read(2).addr.poke(DontCare)
+    dut.read(3).addr.poke(DontCare)
+
+    // verify read data
+    dut.read(0).data.expect(out.readData0)
+    dut.read(1).data.expect(out.readData1)
+    dut.read(2).data.expect(out.readData2)
+    dut.read(3).data.expect(out.readData3)
+  }
+}
+
+class LaForest2W4RInductive(impl: LVTMemory[ParallelWriteMem[SimulationMem], SimulationMem],
+                            spec: Untimed2W4RMemory) extends Mem2W4RProtocol(impl, spec) {
+  require(impl.d.writePorts == 2)
+  require(impl.d.readPorts == 4)
+
+  mapping { (impl, spec) =>
+    forall(0 until impl.d.size.depth.toInt){ addr =>
+      when(spec.valid(addr)) {
+        (0 until 4).foreach { bank =>
+          when(impl.lvt.mem(addr) === bank.U) {
+            assert(spec.mem(addr) === impl.banks(bank).banks(0).mem(addr))
+            assert(spec.mem(addr) === impl.banks(bank).banks(1).mem(addr))
+            assert(spec.mem(addr) === impl.banks(bank).banks(2).mem(addr))
+            assert(spec.mem(addr) === impl.banks(bank).banks(3).mem(addr))
+          }
+        }
+      }
+    }
+  }
+}
+
 class FPGAMemoriesSpec extends FlatSpec {
   "SimulationMemory with 1 Read, 1 Write Port" should "refine its spec" in {
     val data = MemData(MemSize(UInt(32.W), 32), 1, 1)
@@ -71,6 +186,13 @@ class FPGAMemoriesSpec extends FlatSpec {
   }
 
   "Charles Eric LaForest LVT 2W4R memory" should "refine its spec" in {
-    // TODO
+    val data = MemData(MemSize(UInt(32.W), 32), 4, 2)
+    type ImplMem = LVTMemory[ParallelWriteMem[SimulationMem], SimulationMem]
+    def makeSimMem1W1R(size: MemSize) = new SimulationMem(MemData(size, 1, 1))
+    def makeSimMem(data: MemData) = new SimulationMem(data)
+    def makeBanked(size: MemSize) = new ParallelWriteMem(size, makeSimMem1W1R, data.readPorts)
+    def makeLVTMem(size: MemSize) = new LVTMemory(size, makeBanked, makeSimMem, data.writePorts)
+    val p = Elaboration()[ImplMem, Untimed2W4RMemory](makeLVTMem(data.size), new Untimed2W4RMemory(data.size), (impl, spec) => new LaForest2W4RInductive(impl, spec))
+    VerificationProblem.verify(p)
   }
 }
