fc87109916081b46b5c8fa521477749f3aefbdd8
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-05-05 19:40:44 -0700

    BoundedCheck: off by one error in getK

--- src/paso/verification/BoundedCheck.scala
+++ src/paso/verification/BoundedCheck.scala
@@ -111,7 +111,7 @@ class BoundedCheckBuilder(val sys: smt.TransitionSystem, val debugPrint: Boolean
     combined.sortStatesByInitDependencies()
   }
 
-  def getK: Int = steps.length - 1
+  def getK: Int = steps.length
 }
 
 object BoundedCheckBuilder {
