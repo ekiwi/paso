36da5a03cd0128f2b61f7c1e47aad9562200539e
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-05-14 15:46:55 -0700

    aes: fix some wiring bugs in the implementation

--- test/aes/TinyAES128.scala
+++ test/aes/TinyAES128.scala
@@ -32,13 +32,13 @@ class TinyAES128 extends Module {
   val kb = Seq.tabulate(10)(_ => Wire(UInt(128.W)))
 
   val eOuts = k.drop(1) ++ Seq(Wire(UInt(128.W)))
-  val expandKey = k.zip(eOuts).zip(kb).zip(StaticTables.rcon).zipWithIndex.map {
-    case ((((in, out), outDelayed), rcon), ii) => ExpandKey128(rcon, in, out, outDelayed).suggestName(s"a$ii")
+  val expandKey = k.zip(kb).zip(eOuts).zip(StaticTables.rcon).zipWithIndex.map {
+    case ((((in, out), outDelayed), rcon), ii) => ExpandKey128(rcon, in, out, outDelayed).suggestName(s"a${ii+1}")
   }
 
-  val oOuts = s.drop(1) ++ Seq(Wire(UInt(128.W)))
+  val oOuts = s.drop(1)
   val rounds = s.zip(kb).zip(oOuts).zipWithIndex.map {
-    case (((state, key), stateNext), ii) => OneRound(state, key, stateNext).suggestName(s"r$ii")
+    case (((state, key), stateNext), ii) => OneRound(state, key, stateNext).suggestName(s"r${ii+1}")
   }
 
   val finalRound = Module(new FinalRound)
