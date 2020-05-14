3126a5ffdc1fbe7202a8587d73a0644acd1fc6a5
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-05-13 21:15:13 -0700

    examples: add pipelined adder

--- test/examples/PipeliningExamplesSpec.scala
+++ test/examples/PipeliningExamplesSpec.scala
@@ -27,8 +27,37 @@ class RegisterProtocol(impl: Register) extends ProtocolSpec[IdentityNoIdle[UInt]
   }
 }
 
+class Args extends Bundle { val a = UInt(32.W) ; val b = UInt(32.W) }
+class Add32Spec extends UntimedModule {
+  val add = fun("add").in(new Args).out(UInt(32.W)) { (in, out) => out := in.a + in.b }
+}
+
+class PipelinedAdd extends Module {
+  val io = IO(new Bundle{
+    val a = Input(UInt(32.W))
+    val b = Input(UInt(32.W))
+    val out = Output(UInt(32.W))
+  })
+  io.out := RegNext(io.a + io.b)
+}
+
+class PipelinedAddProtocol(impl: PipelinedAdd) extends ProtocolSpec[Add32Spec] {
+  val spec = new Add32Spec
+
+  protocol(spec.add)(impl.io) { (clock, dut, in, out) =>
+    dut.a.set(in.a)
+    dut.b.set(in.b)
+    clock.step() // this is a short-hand for clock.stepAndFork() because we assert an output with no following step
+    dut.out.expect(out)
+  }
+}
+
 class PipeliningExamplesSpec extends FlatSpec {
   "A simple register" should "refine its spec" in {
     Paso(new Register)(new RegisterProtocol(_)).proof()
+  }
+
+  "A pipelined 32-bit adder" should "refine its spec" in {
+    Paso(new PipelinedAdd)(new PipelinedAddProtocol(_)).proof()
   }
 }
