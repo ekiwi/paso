c6c4e4838c0de94b23c779439c2d079c34f0ddc4
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-05-14 20:26:36 -0700

    untimed: sort function arguments by name

--- src/paso/verification/UntimedModel.scala
+++ src/paso/verification/UntimedModel.scala
@@ -37,7 +37,7 @@ object UntimedModel {
 trait IsFunction {
   val sym: smt.Symbol
   val expr: smt.Expr
-  private lazy val args = smt.Context.findSymbols(expr).toList
+  private lazy val args = smt.Context.findSymbols(expr).toList.sortBy(_.id)
   private lazy val funSym = smt.Symbol(sym.id, smt.MapType(args.map(_.typ), expr.typ))
   def functionApp: smt.FunctionApplication = smt.FunctionApplication(funSym, args)
   def functionDef: smt.DefineFun = smt.DefineFun(funSym, args, expr)
@@ -46,7 +46,7 @@ trait IsFunction {
 case class NamedExpr(sym: smt.Symbol, expr: smt.Expr) extends IsFunction
 case class NamedGuardedExpr(sym: smt.Symbol, expr: smt.Expr, guard: smt.Expr) extends IsFunction {
   val guardSym = smt.Symbol(sym.id + ".valid", smt.BoolType)
-  private lazy val guardArgs = smt.Context.findSymbols(guard).toList
+  private lazy val guardArgs = smt.Context.findSymbols(guard).toList.sortBy(_.id)
   private lazy val guardFunSym = smt.Symbol(guardSym.id, smt.MapType(guardArgs.map(_.typ), smt.BoolType))
   def guardFunctionApp: smt.FunctionApplication = smt.FunctionApplication(guardFunSym, guardArgs)
   def guardFunctionDef: smt.DefineFun = smt.DefineFun(guardFunSym, guardArgs, guard)
