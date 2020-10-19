d49b2eac3e03f31882ca189adf6a1b8610b33f8f
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-10-19 13:18:08 -0700

    wip: sym exec

--- src/paso/protocols/SymbolicProtocolInterpreter.scala
+++ src/paso/protocols/SymbolicProtocolInterpreter.scala
@@ -19,7 +19,7 @@ import scala.collection.mutable
  *   in := 1
  *   when c:
  *     in := 2
- *   ``
+ *   ```
  *   results in: `(c => (in = 2)) and (not(c) => (in = 1))`
  *   it might be better to encode this as `in = ite(c, 2, 1)` ...
  * - final input assumptions depend on the same path guards as the next cycle
@@ -48,7 +48,6 @@ case class ProtocolGraph(name: String, cycles: Array[Cycle])
  */
 class SymbolicProtocolInterpreter(protocol: firrtl.CircuitState, stickyInputs: Boolean = false) extends ProtocolInterpreter(protocol) {
   import ProtocolInterpreter.Loc
-  private case class Context()
 
   // predicates that "guard" the current instructions
   private val guards = mutable.ArrayBuffer[smt.BVExpr]()
@@ -69,57 +68,109 @@ class SymbolicProtocolInterpreter(protocol: firrtl.CircuitState, stickyInputs: B
     ProtocolGraph(name, Array())
   }
 
-  private def executeFrom(loc: Loc): Unit = {
+  private def executeFrom(loc: Loc, startCtx: PathCtx): List[PathCtx] = {
+    var ctx = startCtx
     val stmts = getBlock(loc.block).drop(loc.stmt)
     stmts.map{ case (l, s) => parseStmt(s, l) }.foreach {
       case s: DoStep =>
-        onStep(s, loc.stmt + 1 == stmts.length)
-        return // end method here
-      case s: DoSet => onSet(s)
-      case s: DoUnSet => onUnSet(s)
-      case s: DoAssert => onAssert(s)
-      case s: Goto => onGoto(s)
+        // return final ctx
+        return List(onStep(ctx, s, loc.stmt + 1 == stmts.length))
+      case s: DoSet => ctx = onSet(ctx, s)
+      case s: DoUnSet => ctx = onUnSet(ctx, s)
+      case s: DoAssert => ctx = onAssert(ctx, s)
+      case s: Goto => return onGoto(ctx, s)
     }
+    // TODO: if we get here, that means that we reached the end of the protocol
+    List(ctx)
   }
 
-  private def onSet(set: DoSet): Unit = {
+  /** Reading a symbol has different restrictions depending on its kind:
+   * - args: needs to be mapped before it is used as a rvalue; TODO: different effect when used on rhs of set
+   * - rets: should only be used after dependent input args have been mapped (currently just allowed ... :( )
+   * - inputs: (1) if already set: just return value (2) if not set: create new random input (not supported yet)
+   * - outputs: output will be added to read outputs which are used to decide whether an input can be set
+   */
+  private def analyzeRValue(ctx: PathCtx, e: smt.BVExpr, info: ir.Info = ir.NoInfo): (PathCtx, smt.BVExpr) = {
+    val syms = findSymbols(e)
+    filterArgs(syms).foreach { arg =>
+      assert(isMapped(ctx, arg), s"Argument $arg needs to first be bound to an input before reading it!${info.serialize}")
+    }
+    // Note on rets: if their corresponding args are not mapped yet, the test becomes somewhat meaningless
+    //               no matter whether it is concretely or symbolically executed
+    val inputReplacements = filterInputs(syms).map { i => i.name ->
+      ctx.inputValues.get(i.name).map(_.value)
+        .getOrElse(throw new RuntimeException(s"Input $i cannot be read before it is set!${info.serialize}"))
+    }.toMap
+    val newOutputs = filterOutputs(syms).filterNot(o => ctx.outputsRead.contains(o.name))
+      .map(o => o.name -> OutputRead(o.name, info))
+
+    val newExpr = replace(e, inputReplacements).asInstanceOf[smt.BVExpr]
+    val newCtx = ctx.copy(outputsRead = ctx.outputsRead ++ newOutputs)
+    (newCtx, newExpr)
+  }
+
+  private def analyzeSet(input: smt.BVExpr, value: smt.BVExpr): Unit = value match {
+    case l : smt.BVLiteral => // constraint
+    case e : smt.BVSlice => // mapping or constraint
+    case s : smt.BVSymbol => // mapping or constraint
+      
+  }
+
+  private def onSet(ctx: PathCtx, set: DoSet): PathCtx = {
     val value = toSMT(set.expr, inputs(set.loc), allowNarrow = true)
+    value match {
+      case l : smt.BVLiteral =>
+    }
 
-    println(f"SET ${set.loc} <= $value ${set.info.serialize}")
+    //println(f"SET ${set.loc} <= $value ${set.info.serialize}")
   }
-  protected def onUnSet(unset: DoUnSet): Unit = {
-    println(f"UNSET ${unset.loc} ${unset.info.serialize}")
+
+  protected def onUnSet(ctx: PathCtx, unset: DoUnSet): PathCtx = {
+    //println(f"UNSET ${unset.loc} ${unset.info.serialize}")
+    // TODO: check if we remove a mapping of an argument that was read
+    ctx.copy(inputValues = ctx.inputValues - unset.loc)
   }
-  protected def onAssert(a: DoAssert): Unit = {
-    val expr = toSMT(a.expr)
-    println(f"ASSERT $expr ${a.info.serialize}")
+
+  protected def onAssert(ctx: PathCtx, a: DoAssert): PathCtx = {
+    val (c1, expr) = analyzeRValue(ctx, toSMT(a.expr), a.info)
+    //println(f"ASSERT $expr ${a.info.serialize}")
+    c1.copy(asserts = c1.asserts :+ expr)
   }
-  protected def onGoto(g: Goto): Unit = {
-    val cond = toSMT(g.cond)
-    println(f"IF $cond GOTO ${g.conseq} ELSE ${g.alt}")
-    if(cond == smt.True()) {
+
+  protected def onGoto(ctx: PathCtx, g: Goto): List[PathCtx] = {
+    val (c1, cond) = analyzeRValue(ctx, toSMT(g.cond), g.info)
+
+    if(cond == smt.True()) { // just a GOTO, not a branch!
       assert(g.alt == -1)
-      executeFrom(Loc(g.conseq, 0))
-    } else {
-      guards.append(cond)
-      // TODO: check pc feasibility
-      executeFrom(Loc(g.conseq, 0))
-      guards.dropRight(1)
-      guards.append(simplify(smt.BVNot(cond)))
-      // TODO: check pc feasibility
-      executeFrom(Loc(g.alt, 0))
-      guards.dropRight(1)
+      executeFrom(Loc(g.conseq, 0), c1)
+    } else { // actually a branch
+      //println(f"IF $cond GOTO ${g.conseq} ELSE ${g.alt}")
+
+      // execute true path
+      val truPath = simplify(smt.BVAnd(c1.cond, cond))
+      val truCtxs = if(isFeasible(truPath)) {
+        executeFrom(Loc(g.conseq, 0), c1.copy(cond = truPath))
+      } else { List() }
+
+      // execute false path
+      val falsPath = simplify(smt.BVAnd(c1.cond, smt.BVNot(cond)))
+      val falsCtxs = if(isFeasible(falsPath)) {
+        executeFrom(Loc(g.alt, 0), c1.copy(cond = falsPath))
+      } else { List() }
+
+      truCtxs ++ falsCtxs
     }
   }
-  protected def onStep(step: DoStep, isLast: Boolean): Unit = {
+
+  protected def onStep(ctx: PathCtx, step: DoStep, isLast: Boolean): PathCtx = {
     val id = if(isLast) { -1 } else {
       val  i = stepCount ; stepCount += 1
       stepsToProcess.append((step, i))
       i
     }
     val next = Next(guardExpr, step.fork, id)
-    cycle = cycle.copy(next = cycle.next :+ next)
-    println(f"STEP @ ${step.loc} ${step.info.serialize}")
+    //println(f"STEP @ ${step.loc} ${step.info.serialize}")
+    ctx.copy(next = Some(next))
   }
 
   def toSMT(expr: ir.Expression, width: Int = 1, allowNarrow: Boolean = false): smt.BVExpr = {
@@ -128,4 +179,38 @@ class SymbolicProtocolInterpreter(protocol: firrtl.CircuitState, stickyInputs: B
     simplify(e)
   }
   def simplify(e: smt.BVExpr): smt.BVExpr = smt.SMTSimplifier.simplify(e).asInstanceOf[smt.BVExpr]
+  def findSymbols(e: smt.SMTExpr): List[smt.SMTSymbol] = e match {
+    case s: smt.BVSymbol => List(s)
+    case s: smt.ArraySymbol => List(s)
+    case other => other.children.flatMap(findSymbols)
+  }
+  def replace(e: smt.SMTExpr, subs: Map[String, smt.SMTExpr]): smt.SMTExpr = e match {
+    case s : smt.BVSymbol => subs.getOrElse(s.name, s)
+    case other => other.mapExpr(replace(_, subs))
+  }
+  def filterInputs(e: List[smt.SMTSymbol]): Iterable[smt.SMTSymbol] = e.filter(s => inputs.contains(s.name))
+  def filterOutputs(e: List[smt.SMTSymbol]): Iterable[smt.SMTSymbol] = e.filter(s => outputs.contains(s.name))
+  def filterArgs(e: List[smt.SMTSymbol]): Iterable[smt.SMTSymbol] = e.filter(s => args.contains(s.name))
+  def filterRets(e: List[smt.SMTSymbol]): Iterable[smt.SMTSymbol] = e.filter(s => rets.contains(s.name))
+  def isMapped(ctx: PathCtx, arg: smt.SMTSymbol): Boolean = {
+    require(args.contains(arg.name))
+    val allBits = (BigInt(1) << args.asInstanceOf[smt.BVSymbol].width) - 1
+    ctx.mappedArgs.get(arg.name).contains(allBits)
+  }
+  def isFeasible(cond: smt.BVExpr): Boolean = ??? // feasible = satisfiable
+}
+
+
+private case class InputValue(name: String, value: smt.BVExpr, sticky: Boolean, info: ir.Info = ir.NoInfo)
+private case class OutputRead(name: String, info: ir.Info = ir.NoInfo)
+
+private case class PathCtx(
+  cond: smt.BVExpr, asserts: List[smt.BVExpr],
+  inputValues: Map[String, InputValue], outputsRead: Map[String, OutputRead],
+  next: Option[Next]
+) {
+  def mappedArgs: Map[String, BigInt] = ???
+}
+private object PathCtx {
+  def apply(): PathCtx = PathCtx(smt.True(), List(), Map(), Map(), None)
 }
