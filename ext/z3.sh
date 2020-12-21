cfb75e74134eea0377ffa16f97718e338a439d87
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-12-21 14:06:11 -0800

    ext: upgrade to z3 4.8.9

--- ext/z3.sh
+++ ext/z3.sh
@@ -5,12 +5,12 @@
 rm -rf z3-*
 
 # download
-wget https://github.com/Z3Prover/z3/releases/download/z3-4.8.8/z3-4.8.8-x64-ubuntu-16.04.zip
-unzip z3-4.8.8-x64-ubuntu-16.04.zip
+wget https://github.com/Z3Prover/z3/releases/download/z3-4.8.9/z3-4.8.9-x64-ubuntu-16.04.zip
+unzip z3-4.8.9-x64-ubuntu-16.04.zip
 
 # test
-./z3-4.8.8-x64-ubuntu-16.04/bin/z3 test.smt2
+./z3-4.8.9-x64-ubuntu-16.04/bin/z3 test.smt2
 
 # copy
 mkdir -p bin-z3/
-mv z3-4.8.8-x64-ubuntu-16.04/bin/z3 bin-z3/
+mv z3-4.8.9-x64-ubuntu-16.04/bin/z3 bin-z3/
