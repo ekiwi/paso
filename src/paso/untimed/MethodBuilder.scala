ddd84e5d42884199cccc4c6b92b8beac8d64c15e
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-01-26 14:08:57 -0800

    untimed: allow for multiple guards

--- src/paso/untimed/MethodBuilder.scala
+++ src/paso/untimed/MethodBuilder.scala
@@ -6,30 +6,46 @@
 package paso.untimed
 import chisel3._
 
-case class NMethodBuilder(p: MethodParent, n: String, guard: Option[() => Bool] = None) {
+case class NMethodBuilder(p: MethodParent, n: String, guard: List[() => Bool] = List()) {
   def in[I <: Data](inputType: I): IMethodBuilder[I] = IMethodBuilder(p, n, inputType, guard)
   def out[O <: Data](outputType: O): OMethodBuilder[O] = OMethodBuilder(p, n, outputType, guard)
-  def when(cond: => Bool): NMethodBuilder = { require(guard.isEmpty) ; this.copy(guard = Some(() => cond))}
+  def when(cond: => Bool): NMethodBuilder = { this.copy(guard = this.guard :+ (() => cond))}
   def apply(impl: => Unit): NMethod = {
-    val m = NMethod(n, guard.getOrElse(() => true.B), () => impl, p) ; p.addMethod(m) ; m
+    val g = MethodBuilder.mergeGuard(guard)
+    val m = NMethod(n, g, () => impl, p) ; p.addMethod(m) ; m
   }
 }
-case class OMethodBuilder[O <: Data](p: MethodParent, n: String, outputType: O, guard: Option[() => Bool] = None) {
-  def when(cond: => Bool): OMethodBuilder[O] = { require(guard.isEmpty) ; this.copy(guard = Some(() => cond))}
+case class OMethodBuilder[O <: Data](p: MethodParent, n: String, outputType: O, guard: List[() => Bool] = List()) {
+  def when(cond: => Bool): OMethodBuilder[O] = { this.copy(guard = this.guard :+ (() => cond))}
   def apply(impl: O => Unit): OMethod[O] = {
-    val m = OMethod(n, guard.getOrElse(() => true.B), outputType, impl, p) ; p.addMethod(m) ; m
+    val g = MethodBuilder.mergeGuard(guard)
+    val m = OMethod(n, g, outputType, impl, p) ; p.addMethod(m) ; m
   }
 }
-case class IMethodBuilder[I <: Data](p: MethodParent, n : String, inputType: I, guard: Option[() => Bool] = None) {
+case class IMethodBuilder[I <: Data](p: MethodParent, n : String, inputType: I, guard: List[() => Bool] = List()) {
   def out[O <: Data](outputType: O): IOMethodBuilder[I, O] = IOMethodBuilder(p, n, inputType, outputType, guard)
-  def when(cond: => Bool): IMethodBuilder[I] = { require(guard.isEmpty) ; this.copy(guard = Some(() => cond))}
+  def when(cond: => Bool): IMethodBuilder[I] = { this.copy(guard = this.guard :+ (() => cond))}
   def apply(impl: I => Unit): IMethod[I] = {
-    val m = IMethod(n, guard.getOrElse(() => true.B), inputType, impl, p) ; p.addMethod(m) ; m
+    val g = MethodBuilder.mergeGuard(guard)
+    val m = IMethod(n, g, inputType, impl, p) ; p.addMethod(m) ; m
   }
 }
-case class IOMethodBuilder[I <: Data, O <: Data](p: MethodParent, n: String, inputType: I, outputType: O, guard: Option[() => Bool] = None) {
-  def when(cond: => Bool): IOMethodBuilder[I,O] = { require(guard.isEmpty) ; this.copy(guard = Some(() => cond))}
+case class IOMethodBuilder[I <: Data, O <: Data](p: MethodParent, n: String, inputType: I, outputType: O, guard: List[() => Bool] = List()) {
+  def when(cond: => Bool): IOMethodBuilder[I,O] = { this.copy(guard = this.guard :+ (() => cond))}
   def apply(impl: (I, O) => Unit): IOMethod[I,O] = {
-    val m = IOMethod(n, guard.getOrElse(() => true.B), inputType, outputType, impl, p) ; p.addMethod(m) ; m
+    val g = MethodBuilder.mergeGuard(guard)
+    val m = IOMethod(n, g, inputType, outputType, impl, p) ; p.addMethod(m) ; m
+  }
+}
+
+private object MethodBuilder {
+  def mergeGuard[D <: Data](guard: List[() => Bool]): () => Bool = {
+    if(guard.isEmpty) {
+      () => true.B
+    } else if(guard.size == 1) {
+      guard.head
+    } else {
+      () => guard.foldLeft(guard.head())((a,b) => a && b())
+    }
   }
 }
---
+++ test/untimed/UntimedModuleGuardSpec.scala
@@ -0,0 +1,43 @@
+package untimed
+
+import chisel3._
+import org.scalatest.flatspec.AnyFlatSpec
+import paso.UntimedModule
+
+
+
+class UntimedGuardedCounter extends UntimedModule {
+  val count = RegInit(0.U(32.W))
+  // two guards are allowed and will be combined
+  val inc = fun("inc").in(UInt(32.W)).when(count < 5.U).when(count < 4.U) { in =>
+    count := count + in
+  }
+}
+
+class UntimedModuleGuardSpec extends AnyFlatSpec {
+  "a untimed module" should "be allowed to have more than one guard" in {
+    val dut = UntimedModule(new UntimedGuardedCounter).getTester
+
+    // at the beginning the count = 0 => we should be allowed to increment
+    assert(dut.peek("inc_guard") == 1)
+
+    // increment by three
+    dut.poke("inc_enabled", 1)
+    dut.poke("inc_arg", 3)
+    assert(dut.peek("inc_guard") == 1)
+    dut.step()
+
+    // now we should still be able to increment
+    assert(dut.peek("inc_guard") == 1)
+
+    // increment by one
+    dut.poke("inc_enabled", 1)
+    dut.poke("inc_arg", 1)
+    assert(dut.peek("inc_guard") == 1)
+    dut.step()
+
+    // now we should be forbidden to increment since while count < 5.U is true, count < 4.U is false (since count=4)
+    assert(dut.peek("inc_guard") == 0)
+  }
+
+}
