c4b30594bc862e17289b4380cdc9a01eb333f120
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-01-28 18:26:52 -0800

    benchmarks: add bmc and random test for multplier (random is failing for some reason)

--- benchmarks/test/benchmarks/picorv32/PicoRV32Proofs.scala
+++ benchmarks/test/benchmarks/picorv32/PicoRV32Proofs.scala
@@ -4,6 +4,15 @@ import org.scalatest.flatspec.AnyFlatSpec
 import paso._
 
 class PicoRV32Proofs extends AnyFlatSpec {
+  "PicoRV32Mul" should "pass some cycles of random testing" in {
+    Paso(new PicoRV32Mul())(new MulProtocols(_)).randomTest(k=200)
+  }
+
+  "PicoRV32Mul" should "pass some cycles of BMC" in {
+    // NOTE: the kMax is not deep enough to actually verify correct multiplication ...
+    Paso(new PicoRV32Mul())(new MulProtocols(_)).bmc(32)
+  }
+
   "PicoRV32Mul" should "refine its spec" in {
     Paso(new PicoRV32Mul())(new MulProtocols(_)).proof(new ProofCollateral(_, _){
       invariants { dut =>
