fec0153d6446d738ef45147748113b8d21b98400
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-01-29 17:09:54 -0800

    benchmarks: ignore some formal AES checks and add some random tests

--- benchmarks/test/benchmarks/aes/TinyAESFormalChecks.scala
+++ benchmarks/test/benchmarks/aes/TinyAESFormalChecks.scala
@@ -6,11 +6,13 @@ import chisel3._
 
 
 class TinyAESFormalChecks extends AnyFlatSpec {
-  "TinyAES OneRound" should "refine its spec" in {
+  // TODO: seed up proofs!
+
+  "TinyAES OneRound" should "refine its spec" ignore {
     Paso(new OneRound)(new TinyAESRoundProtocol(_)).proof()
   }
 
-  "TinyAES FinalRound" should "refine its spec" in {
+  "TinyAES FinalRound" should "refine its spec" ignore {
     Paso(new FinalRound)(new TinyAESRoundProtocol(_)).proof()
   }
 
@@ -21,11 +23,32 @@ class TinyAESFormalChecks extends AnyFlatSpec {
     }
   }
 
-  "TinyAES128" should "correctly connect all submodules" in {
+  "TinyAES128" should "correctly connect all submodules" ignore {
     Paso(new TinyAES128)(new TinyAESProtocol(_))(new SubSpecs(_, _) {
       replace(impl.finalRound)(new TinyAESRoundProtocol(_)).bind(spec.finalRound)
       impl.rounds.foreach(r => replace(r)(new TinyAESRoundProtocol(_)).bind(spec.round))
       impl.expandKey.zip(spec.expand).foreach{ case (i,s) => replace(i)(new TinyAESExpandKeyProtocol(_)).bind(s) }
     }).proof(Paso.MCYices2)
+  }
+}
+
+class TinyAESRandomChecks extends AnyFlatSpec {
+  "TinyAES OneRound" should "pass a random test" in {
+    Paso(new OneRound)(new TinyAESRoundProtocol(_)).randomTest(1000)
+  }
+
+  "TinyAES FinalRound" should "pass a random test" in {
+    Paso(new FinalRound)(new TinyAESRoundProtocol(_)).randomTest(1000)
+  }
+
+  "TinyAES ExpandKey128" should "pass a random test" in {
+    StaticTables.rcon.foreach { ii =>
+      val rc = ii.U(8.W)
+      Paso(new ExpandKey128(rc))(new TinyAESExpandKeyProtocol(_)).randomTest(100)
+    }
+  }
+
+  "TinyAES128" should "pass a random test" in {
+    Paso(new TinyAES128)(new TinyAESProtocol(_)).randomTest(100)
   }
 }
