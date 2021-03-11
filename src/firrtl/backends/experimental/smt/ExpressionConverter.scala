e8c27c5bb536696d58f918bda140c16e63ea511b
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-03-11 10:01:56 -0800

    firrtl no longer requires a getRandom method

--- src/firrtl/backends/experimental/smt/ExpressionConverter.scala
+++ src/firrtl/backends/experimental/smt/ExpressionConverter.scala
@@ -12,9 +12,7 @@ import maltese.{smt => m}
 
 /** converts between firrtl's internal SMT expr library and the maltese expression library */
 object ExpressionConverter {
-  private case class Context() extends TranslationContext {
-    override def getRandom(width: Int): BVExpr = BVSymbol("Random", width)
-  }
+  private case class Context() extends TranslationContext
   def toMaltese(e: ir.Expression, width: Int, allowNarrow: Boolean): m.BVExpr = {
     implicit val ctx: TranslationContext = Context()
     val firSmt = FirrtlExpressionSemantics.toSMT(e, width, allowNarrow)
