67c28d14a84358c3eab7e5602ef00f8b2e532e48
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-03-26 10:34:34 -0700

    ugraph: copy new signals

--- src/paso/protocols/UGraphPass.scala
+++ src/paso/protocols/UGraphPass.scala
@@ -554,7 +554,7 @@ class ExpandForksPass(protos: Seq[ProtocolInfo], solver: GuardSolver, graphDir: 
     UGraph(merged.name, nodes)
   }
 
-  private def signalNames = List("Active", "Commit", "AllMapped")
+  private def signalNames = List("Active", "Commit", "AllMapped", "Start", "HasCommitted")
   private def replaceProtocolInstances(merged: UGraph, newIds: Seq[(String, Int)], shift: Int): IndexedSeq[UNode] = {
     // replace symbols and signals for all new instances
     val replacements = newIds.zip(protos).map { case ((n, id), p) =>
