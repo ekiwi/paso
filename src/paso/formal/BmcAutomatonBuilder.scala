b6bc29fca1c57f27a2398fdbd31c061e2e3e06d6
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-07-19 17:57:10 -0700

    wip: automaton builder

--- src/paso/formal/BmcAutomatonBuilder.scala
+++ src/paso/formal/BmcAutomatonBuilder.scala
@@ -4,10 +4,12 @@
 
 package paso.formal
 
-import maltese.passes.ReplaceExpressionsWithSignals
 import maltese.{mc, smt}
 import paso.protocols._
-import paso.untimed.MethodInfo
+import chisel3._
+import chisel3.experimental._
+import chisel3.util._
+import firrtl.annotations.PresetAnnotation
 
 import java.nio.file.Path
 
@@ -25,20 +27,104 @@ class BmcAutomatonBuilder(workingDir: Path) {
 
   def run(untimed: UntimedModel, protos: Seq[Proto], invert: Boolean): (mc.TransitionSystem, Int) = {
     require(!invert, "Inversion is currently not supported!")
-    val maxThreads = determineMaxThreads(protos)
+    val maxInstances = determineMaxInstances(protos)
 
-    println(s"MaxThreads: $maxThreads")
+
     ???
   }
 
+  private def pc(p: Proto, i: Int): smt.BVExpr = {
 
-  /**  finds an upper bound for the maximum number of threads that could be active at the same time */
-  private def determineMaxThreads(protos: Seq[Proto]): Int = {
-    val maxMinCycles = protos.map(_.graph).map(ForkAnalysis.run)
-    val minCyclesToFork = maxMinCycles.map(_.minCyclesToFork).min
-    val maxCyclesAfterFork = maxMinCycles.map(_.maxCyclesAfterFork).max
-
-    // TODO: check this formula
-    scala.math.ceil(maxCyclesAfterFork.toDouble / minCyclesToFork.toDouble).toInt + 1
   }
+
+
+  /**  finds an upper bound for the maximum number of protocol instances that could be active at the same time */
+  private def determineMaxInstances(protos: Seq[Proto]): Seq[Int] = {
+    val maxMinCycles = protos.map(_.graph).map(ForkAnalysis.run)
+    maxMinCycles.map { case ForkAnalysis.Result(minCyclesToFork, maxCyclesAfterFork) =>
+      // TODO: check this formula
+      scala.math.ceil(maxCyclesAfterFork.toDouble / minCyclesToFork.toDouble).toInt + 1
+    }
+  }
+}
+
+
+private class BmcAutomaton(protos: Seq[Proto], instances: Seq[Int]) extends Module with RequireAsyncReset {
+  require(protos.nonEmpty)
+  require(protos.size == instances.size)
+  require(instances.forall(_ > 0))
+
+  annotate(new ChiselAnnotation {
+    override def toFirrtl = PresetAnnotation(reset.toTarget)
+  })
+
+  // declare I/Os to/from the untimed module
+  case class MethodIO(guard: Bool, enable: Bool, args: Seq[UInt], rets: Seq[UInt])
+  val methods = protos.map { p =>
+    val prefix = p.name + "_"
+    val enable = IO(Output(Bool())).suggestName(prefix + "enable")
+    enable := false.B
+    MethodIO(
+      IO(Input(Bool())).suggestName(prefix + "guard"),
+      enable,
+      p.info.args.map{ case (n,b) => IO(Output(UInt(b.W))).suggestName(prefix + n) }.toSeq,
+      p.info.rets.map{ case (n,b) => IO(Input(UInt(b.W))).suggestName(prefix + n) }.toSeq,
+    )
+  }
+
+  // forking off a new protocol
+  val forking = Wire(Bool())
+  when(forking) {
+    // check which protocols can be executed
+    val guards = Cat(methods.map(_.guard))
+
+    // at least one protocol needs to be able to be executed
+    verification.assert(guards.orR(), "No guard enabled! Deadlock!")
+
+    // pick an active protocol
+    val starting = if(protos.size == 1) { 1.U(1.W) } else {
+      val pick = IO(Input(UInt(log2Ceil(protos.size).W))).suggestName("pickProtocol")
+      verification.assume(pick < protos.size.U, "Protocol id needs to be in range")
+      pick
+    }
+    // the protocol that was picked should be active
+    verification.assume((UIntToOH(starting) & guards) =/= 0.U, "Picked protocol is active!")
+
+    // execute the transaction that we picked
+    methods.zipWithIndex.foreach { case (m, i) => m.enable := starting === i.U }
+
+
+
+  }
+
+
+  // generate registers for all protocol instances
+  case class ProtoState(p: Proto, i: Int, pc: UInt, args: Seq[UInt], rets: Seq[UInt])
+  val states = protos.zip(instances).flatMap { case (proto, inst) =>
+    val pcBits = List(log2Ceil(proto.graph.nodes.size), 1).max
+    (0 until inst).map { i =>
+      val prefix = s"${proto.name}_${i}_"
+      val key = (proto.name, i)
+      val states =ProtoState(proto, i,
+         pc = RegInit(0.U(pcBits.W)).suggestName(prefix + "pc"),
+         args = proto.info.args.map{ case (n, b) => Reg(UInt(b.W)).suggestName(prefix + n) }.toSeq,
+         rets = proto.info.rets.map{ case (n, b) => Reg(UInt(b.W)).suggestName(prefix + n) }.toSeq,
+      )
+
+
+      key -> states
+    }
+  }.toMap
+
+  // we are in a fork state if this is the first cycle (i.e. no protocols are active)
+  // or if any of the protocols wants to fork
+  val noProtocolActive = states.values.map(_.pc === 0.U).reduce(_ && _)
+
+
+
+
+}
+
+private abstract class Builder {
+
 }
